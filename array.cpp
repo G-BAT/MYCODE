2f275d353f97d4174df1883fbf1b5aca1a24b8e0
BAT <batman.02970@gmail.com>
2025-02-16 12:43:30 +0530

    added target sums

--- array.cpp
+++ array.cpp
@@ -135,7 +135,61 @@ int main(){
     }
 }
 -------------------------------------------------------------------
-TARGET SUM
+TARGET SUM(2-PAIR)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+
+int v[]={1,2,3,4,5};
+int n=sizeof(v)/sizeof(v[0]);
+int t=9;
+
+
+for(int i=0; i<n; ++i){
+    for(int j=1+i; j<n; ++j){
+        if(v[i]+v[j]==t){
+            cout << "index:" <<  i << " " << j;
+        }
+    }
+}
+
+}
+------------------------------------------------------------------
+TARGET SUM(2-PAIR)(TWO POINTER PAIR)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int v[]={1,2,3,4,5};
+    int n=sizeof(v)/sizeof(v[0]);
+    int t=9;
+    int i=0,j=n-1;
+    
+    while(i<j){
+        int p=v[i]+v[j];
+    
+        if(p>t){
+            --j;
+        }
+        else if(p<t){
+            ++i;
+        }
+        else{
+            cout << "index:" <<  i << " " << j;
+            break;
+        }
+    }
+
+}
+--------------------------------------------------------------------
+TARGET SUM(3-PAIR)
 
 #include <iostream>
 #include <vector>
